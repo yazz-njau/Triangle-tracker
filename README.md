15eac732b09460bad11d7a2c4e07d8ece5719f91
Robert njau <48471325+yazz-njau@users.noreply.github.com>
2019-03-25 08:39:49 +0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# Triangle-tracker
+    TRIANGLE TRACKER PROGRAM
+This project have been designed to track the types of triangles are there when you enter the different values
+    THE AUTHOR
+This project is written by Robert Njau still as a student at moringa where he is in the process of learning about codding
+     THE TECHNOLOGIES USED
+The programmer has used a total of three different techs ie HTML ,CSS, javascript     
---
+++ triangle.js
@@ -0,0 +1,18 @@
+function getResult() {
+}  var A= parseInt(prompt("A");
+  var B= parseInt(prompt("B");
+  var C= parseInt(prompt("C");
+  var determine = function(A,B, C)
+  if (A=== B && B ==== C && A===C){
+    alert ("Equilateral");
+ }
+ else if  ((A ==B) (A ! = C && B !== C));{
+    alert("Iscoceless");
+    }
+    if else((A+ B) <= C || (A + C) <= B || (B + C) <= A) {
+   alert("Not a definite triangle");
+   }
+   else (A!==B && A!==C && B!==C){
+   alert("Scalene");
+  }
+};
